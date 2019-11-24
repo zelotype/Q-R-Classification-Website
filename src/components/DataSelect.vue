05f107b6e622cb408c03d0bbe2155b5f30629b1c
Kawisara Bunyen <zelotype.3@gmail.com>
2019-11-24 23:19:18 +0700

    Add data from select column done but still have bug

--- src/components/DataSelect.vue
+++ src/components/DataSelect.vue
@@ -6,7 +6,7 @@
           v-for="col in columns"
           :key="col"
           class="border text-center"
-          v-bind:class="{'bg-green-500':isSelect }"
+          v-bind:class="{ 'bg-green-500': isSelect }"
           v-on:click="selectCol(col)"
         >
           {{ col }}
@@ -14,7 +14,7 @@
       </tr>
       <tr v-for="data in dataFromUser" :key="data" class="border">
         <td v-for="col in columns" :key="col" class="border pl-2">
-          {{ data.row[col] }}
+          {{ data[col] }}
         </td>
       </tr>
     </table>
@@ -31,34 +31,36 @@ export default {
     return {
       dataFromUser: [
         {
-          row: {
-            A: "fox",
-            B: "delete",
-            C: "osu",
-            D: "haru",
-            F: "dano"
-          }
+          A: "fox",
+          B: "delete",
+          C: "osu",
+          D: "haru",
+          F: "dano"
         },
         {
-          row: {
-            A: "joe",
-            B: "kris",
-            C: "nami",
-            D: "micky",
-            F: "panda"
-          }
+          A: "joe",
+          B: "kris",
+          C: "nami",
+          D: "micky",
+          F: "panda"
         },
-        { row: { A: "garfield", B: "tshirt" } }
+        { A: "garfield", B: "tshirt" }
       ],
       dataSelect: [],
-      isSelect: false
+      isSelect: false,
+      count: 0
     };
   },
   methods: {
     selectCol: function selectCol(col) {
-      for (var i in this.dataFromUser) {
-        this.dataSelect.push(this.dataFromUser[i].row[col]);
-      }
+      let groupBy = (array, key) => {
+        return array.reduce((result, currentValue) => {
+          (result[key] = result[key] || []).push(currentValue[key]);
+          return result;
+        }, {});
+      };
+      var json = groupBy(this.dataFromUser, col);
+      this.dataSelect.push(json);
     }
   },
   computed: {
@@ -66,7 +68,7 @@ export default {
       if (this.dataFromUser.length == 0) {
         return [];
       }
-      return Object.keys(this.dataFromUser[0].row);
+      return Object.keys(this.dataFromUser[0]);
     }
   }
 };
