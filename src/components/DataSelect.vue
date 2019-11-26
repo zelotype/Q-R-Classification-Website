3ccc3108a5c5fb9649334039bc0b6db34149002a
Kawisara Bunyen <zelotype.3@gmail.com>
2019-11-26 16:59:55 +0700

    DataSelect complete

--- src/components/DataSelect.vue
+++ src/components/DataSelect.vue
@@ -6,21 +6,15 @@
           v-for="col in columns"
           :key="col"
           class="border text-center"
-          v-bind:class="{ 'bg-green-500': isSelect }"
           v-on:click="selectCol(col)"
         >
           {{ col }}
         </th>
       </tr>
-      <tr v-for="data in dataFromUser" :key="data" class="border">
-        <td v-for="col in columns" :key="col" class="border pl-2">
-          {{ data[col] }}
-        </td>
-      </tr>
     </table>
 
     <div>
-      {{ dataSelect }}
+      {{ columnSelect }}
     </div>
   </div>
 </template>
@@ -29,51 +23,23 @@
 export default {
   data() {
     return {
-      dataFromUser: [
-        {
-          A: "fox",
-          B: "delete",
-          C: "osu",
-          D: "haru",
-          F: "dano"
-        },
-        {
-          A: "joe",
-          B: "kris",
-          C: "nami",
-          D: "micky",
-          F: "panda"
-        },
-        { A: "garfield", B: "tshirt" }
-      ],
-      dataSelect: [],
-      isSelect: false,
-      count: 0
+      columnFromData: ["A", "B", "C", "D", "E", "F", "G"],
+      columnSelect: []
     };
   },
   methods: {
     selectCol: function selectCol(col) {
-      let groupBy = (array, key) => {
-        return array.reduce((result, currentValue) => {
-          (result[key] = result[key] || []).push(currentValue[key]);
-          return result;
-        }, {});
-      };
-      var json = groupBy(this.dataFromUser, col)
-      var dataSelectKey = this.dataSelect.map((k)=> Object.keys(k))
-      console.log(dataSelectKey)
-      if(!(dataSelectKey.includes(col))){
-        console.log(json)
-        this.dataSelect.push(json)
+      if(!(this.columnSelect.includes(col))){
+        this.columnSelect.push(col)
       }
     }
   },
   computed: {
     columns: function columns() {
-      if (this.dataFromUser.length == 0) {
+      if (this.columnFromData.length == 0) {
         return [];
       }
-      return Object.keys(this.dataFromUser[0]);
+      return this.columnFromData;
     }
   }
 };
